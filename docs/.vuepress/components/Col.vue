f195a7a78b509a09444861da216e0e031f2a797f
Emily Carey <emilyrosinacarey@gmail.com>
2022-04-22 22:21:27 +0100

    structure: add col and row components

---
+++ docs/.vuepress/components/Col.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="['col', {}]">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Col',
+}
+</script>
+
+<style lang="scss" scoped>
+.col {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ docs/.vuepress/components/Row.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="['row', {}]">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Row',
+}
+</script>
+
+<style lang="scss" scoped>
+.row {
+  display: flex;
+  flex-direction: row;
+
+  .col {
+    flex: 1;
+  }
+}
+</style>
