d135afa9a7836b9685933543db4a59c4ff7d6a5a
Emily Carey <emilyrosinacarey@gmail.com>
2022-05-11 19:19:04 +0100

    dates menu: add component for dates menu + update docs page

---
+++ docs/.vuepress/components/GsfDatesMenu.vue
@@ -0,0 +1,309 @@
+<template>
+  <div class="dates-menu">
+    <div class="dates-menu__header">
+      <img src="/logo.png">
+      <span>Apply dates to your query</span>
+      <GsfIcon
+        icon="close"
+        size="auto"/>
+    </div>
+    <div
+      :empty="isEmpty"
+      class="dates-menu__row dates-menu__row--example">
+      <span class="title">archived</span>
+      <span
+        :disabled="!isValid"
+        class="button button--add">
+        Add
+      </span>
+      <template v-if="isRange">
+        <span
+          aria-label="from"
+          :invalid="!isValid"
+          class="input input-1 input--from">
+          {{ dateShown.from }}
+        </span>
+        <span
+          aria-label="to"
+          :invalid="!isValid"
+          class="input input-2 input--to">
+          {{ dateShown.to }}
+        </span>
+      </template>
+      <span
+        v-else
+        aria-label="on"
+        :invalid="!isValid"
+        class="input input-1 input--on">
+        {{ dateShown.on }}
+      </span>
+      <GsfIcon
+        v-if="!isRange"
+        icon="calendar"
+        size="16"/>
+      <GsfIcon
+        v-else
+        icon="calendarRange"
+        size="16"/>
+      <GsfIcon
+        icon="trash"
+        size="18"/>
+    </div>
+    <div class="dates-menu__row">
+      <span>closed</span>
+      <span class="button button--add">Add</span>
+    </div>
+    <div class="dates-menu__row">
+      <span>created</span>
+      <span class="button button--add">Add</span>
+    </div>
+    <div class="dates-menu__row">
+      <span>merged</span>
+      <span class="button button--add">Add</span>
+    </div>
+    <div class="dates-menu__row">
+      <span>updated</span>
+      <span class="button button--add">Add</span>
+    </div>
+    <div class="dates-menu__footer">
+      <span class="button button--reset">Reset</span>
+      <span
+        :disabled="!isValid"
+        class="button button--apply">
+        Apply
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GsfDatesMenu',
+  props: {
+    isRange: {
+      type: Boolean,
+      default: false,
+    },
+    isEmpty: {
+      type: Boolean,
+      default: false,
+    },
+    isValid: {
+      type: Boolean,
+      default: false,
+    },
+    populatedDates: {
+      type: Object,
+      default: () => [],
+    }
+  },
+  computed: {
+    dateShown () {
+      const { populatedDates } = this
+      const empty = 'dd/mm/yyyy'
+      const firstDate = '01/04/2020'
+      const endDate = '01/10/2021'
+      return {
+        on: populatedDates.includes('on') ? firstDate : empty,
+        from: populatedDates.includes('from') ? firstDate : empty,
+        to: populatedDates.includes('to') ? endDate : empty,
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@styles/vars";
+
+:root {
+  --gsf-dates-menu-bg: #f4f4f4;
+  --gsf-dates-menu-border: #bababa;
+  --gsf-dates-menu-button-outline: #{$gh-search-text};
+  --gsf-dates-menu-input-color: #{$gh-search-text};
+  --gsf-dates-menu-trash-color: #{$error};
+}
+
+html.dark {
+  --gsf-dates-menu-bg: #{$grey-dark};
+  --gsf-dates-menu-border: #{$gh-search-text};
+  --gsf-dates-menu-button-outline: #{$grey-lightest};
+  --gsf-dates-menu-input-color: #{$grey-lightest};
+  --gsf-dates-menu-trash-color: #f9606c;
+}
+</style>
+
+<style lang="scss" scoped>
+@import "@styles/vars";
+
+.dates-menu,
+.dates-menu * {
+  transition: all 0.1s ease;
+  user-select: none;
+}
+
+.button,
+.dates-menu__header .gsf-close-icon,
+.dates-menu__row .gsf-icon {
+  cursor: pointer;
+}
+
+.button {
+  padding: 4px 10px;
+  border: 1px solid var(--gsf-dates-menu-button-outline);
+  border-radius: 4px;
+  font-size: 13px;
+  font-weight: 400;
+
+  + .button {
+    margin-left: 4px;
+  }
+
+  &--apply {
+    border: none;
+    background: $success;
+    color: #fff;
+
+    &[disabled="true"] {
+      background: $disabled;
+      cursor: default;
+    }
+  }
+
+  &--add {
+    &[disabled="true"] {
+      border-color: $disabled;
+      color: $disabled;
+      cursor: default;
+    }
+  }
+}
+
+.dates-menu {
+  width: 20em;
+  max-width: 20em;
+  border: 1px solid var(--gsf-dates-menu-border);
+  border-radius: 6px;
+  background: var(--gsf-dates-menu-bg);
+  color: var(--c-text);
+  font-size: 120%;
+
+  &__header {
+    display: flex;
+    align-items: center;
+    padding: 0.75em 0.75em 0.75em 1.25em;
+    font-size: 0.85em;
+    font-weight: 600;
+
+    img {
+      height: 1.1em;
+      margin-right: 0.5em;
+    }
+
+    .gsf-icon {
+      width: 1.25em;
+      height: 1.25em;
+      margin-left: auto;
+    }
+  }
+
+  &__row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.75em 1.25em;
+    border-top: 1px solid var(--gsf-dates-menu-border);
+    font-size: 0.85em;
+    font-weight: 600;
+
+    .button {
+      padding: 0.25em 0.5em;
+      font-size: 0.75em;
+      font-weight: 600;
+    }
+
+    &--example {
+      display: grid;
+      grid-template:
+        "title    .         button    button"     2em
+        "input-1  input-2   cal-icon  trash-icon" 1.35em;
+      grid-template-columns: 1fr 1fr 2em 1.25em;
+
+      .title {
+        grid-area: title;
+        align-self: flex-start;
+      }
+
+      .button {
+        grid-area: button;
+        justify-self: flex-end;
+        align-self: flex-start;
+      }
+
+      .input {
+        position: relative;
+        padding: 0.25em 0.3em 0.2em;
+        border: 1px solid var(--gsf-dates-menu-input-color);
+        border-radius: 0.25em;
+        color: var(--gsf-dates-menu-input-color);
+        font-size: 0.8em;
+        font-weight: 500;
+
+        &::before {
+          content: attr(aria-label);
+          display: inline-block;
+          position: absolute;
+          top: -0.65em;
+          left: 0.65em;
+          background: var(--gsf-dates-menu-bg);
+          font-size: 0.75em;
+        }
+
+        &[invalid="true"] {
+          border-color: #d47a51;
+          color: #d47a51;
+        }
+      }
+
+      .input-1 {
+        grid-area: input-1;
+      }
+
+      .input-2 {
+        grid-area: input-2;
+        margin-left: 0.5em;
+      }
+
+      .gsf-icon {
+        grid-area: cal-icon;
+        justify-self: flex-end;
+      }
+
+      .gsf-icon:last-child {
+        grid-area: trash-icon;
+        color: var(--gsf-dates-menu-trash-color);
+      }
+
+      &[empty="true"] {
+        grid-template-rows: 1.35em 0;
+
+        .title {
+          align-self: center;
+        }
+
+        .input,
+        .gsf-icon {
+          display: none;
+        }
+      }
+    }
+  }
+
+  &__footer {
+    display: flex;
+    justify-content: flex-end;
+    padding: 0.5em;
+    border-top: 1px solid var(--gsf-dates-menu-border);
+  }
+}
+</style>
--- docs/elements/dates-menu/index.md
+++ docs/elements/dates-menu/index.md
@@ -7,41 +7,83 @@ tags:
 
 # Dates Menu
 
+## Query a date
+
+<Row>
+  <Col>
+    <ol>
+      <li>Choose a date type to add to your query and click the <GsfButton outline sm>Add</GsfButton> button.</li>
+      <li>Click the <GsfIcon icon="calendar" color="#e1734c"/> within the <b>on</b> input, to pick the date you wish to apply.</li>
+      <li>Click <GsfButton theme="success" sm>Apply</GsfButton></li>
+    </ol>
+  </Col>
+  <Col>
+    <GsfDatesMenu is-valid :populated-dates="['on']"/>
+  </Col>
+</Row>
+
 ## Query after date
 
-1. Click the <GsfIcon icon="calendar" color="#e1734c"/> to the far right, to change the date filter to a range.
-1. You will now see that the icon has changed to a <GsfIcon icon="calendarRange" color="#e1734c"/> and you will have 2 date inputs to populate.
-1. Select a date for the **From** input.
-1. Leave the **To** input empty.
-1. Click <GsfButton theme="success" sm>Apply</GsfButton>
+<Row>
+  <Col>
+    <ol>
+      <li>Click the <GsfIcon icon="calendar" color="#e1734c"/> to the far right, to change the date filter to a range.</li>
+      <li>You will now see that the icon has changed to a <GsfIcon icon="calendarRange" color="#e1734c"/> and you will have 2 date inputs to populate.</li>
+      <li>Select a date for the **From** input.</li>
+      <li>Leave the **To** input empty.</li>
+      <li>Click <GsfButton theme="success" sm>Apply</GsfButton></li>
+    </ol>
+  </Col>
+  <Col>
+    <GsfDatesMenu is-valid is-range :populated-dates="['from']"/>
+  </Col>
+</Row>
 
 
 ## Query before date
 
-1. Click the <GsfIcon icon="calendar" color="#e1734c"/> icon to the far right, to change the date filter to a range.
-1. You will now see that the icon has changed to a <GsfIcon icon="calendarRange" color="#e1734c"/> and you will have 2 date inputs to populate.
-1. Select a date for the **To** input.
-1. Leave the **From** input empty.
-1. Click <GsfButton theme="success" sm>Apply</GsfButton>
-
+<Row>
+  <Col>
+    <ol>
+      <li>Click the <GsfIcon icon="calendar" color="#e1734c"/> icon to the far right, to change the date filter to a range.</li>
+      <li>You will now see that the icon has changed to a <GsfIcon icon="calendarRange" color="#e1734c"/> and you will have 2 date inputs to populate.</li>
+      <li>Select a date for the **To** input.</li>
+      <li>Leave the **From** input empty.</li>
+      <li>Click <GsfButton theme="success" sm>Apply</GsfButton></li>
+    </ol>
+  </Col>
+  <Col>
+    <GsfDatesMenu is-valid is-range :populated-dates="['to']"/>
+  </Col>
+</Row>
 
 ## Query between 2 dates
 
-1. Click the <GsfIcon icon="calendar" color="#e1734c"/> to the far right, to change the date filter to a range.
-1. You will now see that the icon has changed to a <GsfIcon icon="calendarRange" color="#e1734c"/> and you will have 2 date inputs to populate.
-1. Select dates for both inputs.
-1. Click <GsfButton theme="success" sm>Apply</GsfButton>
-
-
-## Query a date
-
-1. Choose a date type to add to your query and click the <GsfButton outline sm>Add</GsfButton> button.
-1. Click the <GsfIcon icon="calendar" color="#e1734c"/> within the **On** input, to pick the date you wish to apply.
-1. Click <GsfButton theme="success" sm>Apply</GsfButton>
-
+<Row>
+  <Col>
+    <ol>
+      <li>Click the <GsfIcon icon="calendar" color="#e1734c"/> to the far right, to change the date filter to a range.</li>
+      <li>You will now see that the icon has changed to a <GsfIcon icon="calendarRange" color="#e1734c"/> and you will have 2 date inputs to populate.</li>
+      <li>Select dates for both inputs.</li>
+      <li>Click <GsfButton theme="success" sm>Apply</GsfButton></li>
+    </ol>
+  </Col>
+  <Col>
+    <GsfDatesMenu is-valid is-range :populated-dates="['from', 'to']"/>
+  </Col>
+</Row>
 
 ## Remove date query
 
-1. Identify the date filter you wish to remove.
-1. Click the <GsfIcon icon="trash" color="#CB2431"/> on the far right of the date filter.
-1. Click <GsfButton theme="success" sm>Apply</GsfButton>
+<Row>
+  <Col>
+    <ol>
+      <li>Identify the date filter you wish to remove.</li>
+      <li>Click the <GsfIcon icon="trash" color="#CB2431"/> on the far right of the date filter.</li>
+      <li>Click <GsfButton theme="success" sm>Apply</GsfButton></li>
+    </ol>
+  </Col>
+  <Col>
+    <GsfDatesMenu is-valid is-empty/>
+  </Col>
+</Row>
